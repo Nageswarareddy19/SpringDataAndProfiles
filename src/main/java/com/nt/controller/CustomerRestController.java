8169dcc69a5c9f06c61deb6191b82964b766eb9b
Nageswarareddy19 <nageswarareddykandikonda@gmail.com>
2019-11-25 14:20:00 +0530

    logging is updated

--- src/main/java/com/nt/controller/CustomerRestController.java
+++ src/main/java/com/nt/controller/CustomerRestController.java
@@ -2,6 +2,8 @@ package com.nt.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nt.exceptions.CustomerNotFoundException;
@@ -19,28 +22,45 @@ import com.nt.service.CustomerService;
 @RestController
 public class CustomerRestController {
 
+	Logger logger = LoggerFactory.getLogger(CustomerRestController.class);
+
+	public CustomerRestController() {
+		logger.debug("CustomerRestController-Instantiated");
+		logger.info("CustomerRestController-Instantiated");
+	}
+
 	@Autowired
 	private CustomerService custService;
-
+	
 	@PostMapping(value = "/add", consumes = { "application/json", "application/xml" })
+	
 	public ResponseEntity<String> addCustomer(@RequestBody Customer c) {
+		logger.debug("addCustomer() method is called");
 		boolean isSaved = custService.addCustomer(c);
 		if (isSaved) {
 			String res = "Customer details sucessfully added";
+			logger.debug("addCustomer() method is ended");
+			logger.info("addCustomer() method is sucessfully completed");
 			return new ResponseEntity<String>(res, HttpStatus.CREATED);
+
 		} else {
 			String res = "Customer details is not added";
+			logger.error("Internal server error");
 			return new ResponseEntity<String>(res, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 	}
 
-	@GetMapping(value = "/get/{customerID}", produces = { "application/json", "application/xml" })
-	public ResponseEntity<Customer> getCustomerBydId(@PathVariable Integer customerID) {
+	@GetMapping(value = "/get", produces = { "application/json", "application/xml" })
+	public ResponseEntity<Customer> getCustomerBydId(@RequestParam Integer customerID) {
+		logger.debug("getCustomerById() method is called");
 		Customer cust = custService.getCustomerDetailsByID(customerID);
 		if (cust != null) {
+			logger.debug("getCustomerById() method is ended");
+			logger.info("getCustomerById() method is sucessfully executed");
 			return new ResponseEntity<Customer>(cust, HttpStatus.OK);
 		} else {
+			logger.error("CustomerNotFoundException is occurred");
 			throw new CustomerNotFoundException("Customer id is not availble");
 		}
 
@@ -48,11 +68,14 @@ public class CustomerRestController {
 
 	@GetMapping(value = "/getEmail/{email}", produces = { "application/json", "application/xml" })
 	public ResponseEntity<Customer> getCustomerDetailsByEmail(@PathVariable String email) {
-
+			logger.debug("getCustomerDetailsByEmail() method is called");
 		Customer c = custService.findByEmail(email);
 		if (c != null) {
+			logger.debug("getCustomerDetailsByEmail() method is ended");
+			logger.info("getCustomerDetailsByEmail() method is sucessfully executed");
 			return new ResponseEntity<Customer>(c, HttpStatus.OK);
 		} else {
+			logger.error("CustomerNotFoundException is occurred");
 			throw new CustomerNotFoundException("Customer Email is not availabe");
 		}
 
@@ -60,12 +83,15 @@ public class CustomerRestController {
 
 	@GetMapping(value = "/getEmails", produces = { "application/json", "application/xml" })
 	public ResponseEntity<List<String>> getAllMails() {
-
+		logger.debug("getAllMails() method is called");
 		List<String> list = custService.findAllEmails();
 
 		if (!list.isEmpty()) {
+			logger.debug("getAllMails() method is ended");
+			logger.info("getAllMails() method is sucessfully executed");
 			return new ResponseEntity<List<String>>(list, HttpStatus.OK);
 		} else {
+			logger.error("EmailsNotFoundException is raised");
 			throw new EmailsNotFoundException("Emails are not found");
 		}
 
--- src/main/java/com/nt/service/CustomerServiceImpl.java
+++ src/main/java/com/nt/service/CustomerServiceImpl.java
@@ -1,21 +1,27 @@
 package com.nt.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nt.entity.CustomerEntity;
-import com.nt.exceptions.CustomerNotFoundException;
 import com.nt.model.Customer;
 import com.nt.repository.CustomerRepository;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
+	Logger logger = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
+	public CustomerServiceImpl() {
+		logger.debug("CustomerServiceImpl:instantiated");
+		logger.info("CustomerServiceImpl:instantiated");
+	}
 
 	@Autowired
 	private CustomerRepository custRepo;
@@ -23,10 +29,13 @@ public class CustomerServiceImpl implements CustomerService {
 	@SuppressWarnings("unlikely-arg-type")
 	@Override
 	public boolean addCustomer(Customer c) {
+		logger.debug("addCustomer() method is started");
 		CustomerEntity entity = new CustomerEntity();
 		BeanUtils.copyProperties(c, entity);
 
 		entity = custRepo.save(entity);
+		logger.debug("addCustomer() method is ended");
+		logger.info("addCustomer() method is sucessfully executed");
 
 		return entity.getCustomerId() > 0;
 
@@ -34,35 +43,43 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Override
 	public Customer getCustomerDetailsByID(Integer customerID) {
+		logger.debug("getCustomerDetailsByID() method is started");
 		Optional<CustomerEntity> optEntity = custRepo.findById(customerID);
 
 		if (optEntity.isPresent()) {
 			CustomerEntity customerEntity = optEntity.get();
 			Customer model = new Customer();
 			BeanUtils.copyProperties(customerEntity, model);
+			logger.debug("getCustomerDetailsByID() method is ended");
 			return model;
-		}
 
+		}
+		logger.info("getCustomerDetailsByID() method is sucessfully executed ");
 		return null;
 
 	}
 
 	@Override
 	public Customer findByEmail(String email) {
+		logger.debug("findByEmail() method is started");
 		CustomerEntity entity = custRepo.findByEmail(email);
 
 		if (entity != null) {
 			Customer model = new Customer();
 			BeanUtils.copyProperties(entity, model);
+			logger.debug("findByEmail() method is ended");
 			return model;
 		}
-
+		logger.info("findByEmail() method is sucessfully executed");
 		return null;
 	}
 
 	@Override
 	public List<String> findAllEmails() {
+		logger.debug("findAllEmails() method is started");
 		List<String> findAllMails = custRepo.findAllMails();
+		logger.debug("findAllEmails() method is ended");
+		logger.info("findAllEmails() method is executed");
 		return findAllMails;
 	}
 }
